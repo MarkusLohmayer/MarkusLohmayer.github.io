8ab28421b7246dd79ea1a985f5bb9eaec6a0435c
Markus Lohmayer <markus.lohmayer@gmail.com>
2025-07-18 06:42:19 +0200

    nix, nodejs

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Grounded-Roots.de Website built with Svelte";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    systems.url = "github:nix-systems/default";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      systems = import inputs.systems;
+
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs
+            ];
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              nixfmt.package = pkgs.nixfmt-rfc-style;
+              dos2unix.enable = true;
+              prettier.enable = true;
+            };
+          };
+        };
+    };
+}
